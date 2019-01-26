2e3eb6b22ab77b675b79c0af1bedc9285f2d9b0c
Linda Chang <lindachang186@gmail.com>
2019-01-26 20:14:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,15 +67,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  sql = <<-SQL
-  SELECT name
-  FROM movies
-  WHERE release_date > 2002
-  SQL
-  DB[:conn].execute(sql, name).map do |row|
-    self.new_from_db(row)
-    end
-  end 
+  Movie.where(release_date > 2002)
 end
 
 def can_be_found_updated_and_saved
